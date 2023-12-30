43ae4aa46f5f28e8e2fbe3eddb4897e464cce3db
Jose Britto <jchdb7@gmail.com>
2023-12-30 13:57:13 -0300

    feat: Adicionado entidades, dbsets e migrations

---
+++ BlogWebApi.Domain/Entities/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BlogWebApi.Domain.Entities
+{
+    public class Post : BaseEntity
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public Author Author { get; set; }
+    }
+}
