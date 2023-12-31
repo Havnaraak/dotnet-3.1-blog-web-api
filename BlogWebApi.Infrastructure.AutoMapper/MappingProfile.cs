f557ce76996060025f98101827edd358e24eef3b
Jose Britto <jchdb7@gmail.com>
2023-12-31 11:17:24 -0300

    feat: adicionado summary da configuração do AutoMapper para explicação dos métodos

--- BlogWebApi.Infrastructure.AutoMapper/MappingProfile.cs
+++ BlogWebApi.Infrastructure.AutoMapper/MappingProfile.cs
@@ -14,6 +14,10 @@ namespace BlogWebApi.Infrastructure.AutoMapper
             ApplyMappingsFromAssembly(typeof(IMapTo<>).Assembly);
         }
 
+        /// <summary>
+        /// Responsável por buscar os types disponíveis no assembly da interface de map
+        /// </summary>
+        /// <param name="assembly">Assembly para ser usado na busca dos types</param>
         private void ApplyMappingsFromAssembly(Assembly assembly)
         {
             var assemblyTypes = assembly.GetExportedTypes();
@@ -22,6 +26,12 @@ namespace BlogWebApi.Infrastructure.AutoMapper
             AddMappingForTypes(this, assemblyTypes, typeof(IMapTo<>));
         }
 
+        /// <summary>
+        /// Responsável por percorrer os tipos do assembly e identificar os tipos que herdam da interface de map
+        /// </summary>
+        /// <param name="profile">Profile do automapper</param>
+        /// <param name="types">Lista com types dentro do assembly para varredura</param>
+        /// <param name="interfaceType">Tipo da interface que será buscada</param>
         private static void AddMappingForTypes(Profile profile, IEnumerable<Type> types, Type interfaceType)
         {
             var filteredTypes = types
@@ -34,6 +44,13 @@ namespace BlogWebApi.Infrastructure.AutoMapper
             }
         }
 
+
+        /// <summary>
+        /// Responsável por criar a instância do tipo e chamar o método Mapping que é reponsável pelo CreateMap
+        /// </summary>
+        /// <param name="profile">Profile do automapper</param>
+        /// <param name="type">Tipo da classe que será adicionada</param>
+        /// <param name="interfaceName">Nome da interface para adicionar</param>
         private static void AddMappingForType(Profile profile, Type type, string interfaceName)
         {
             const string methodName = "Mapping";
@@ -42,6 +59,12 @@ namespace BlogWebApi.Infrastructure.AutoMapper
             methodInfo?.Invoke(instance, new object[] { profile });
         }
 
+        /// <summary>
+        /// Valida se o tipo informado herda a interface de map
+        /// </summary>
+        /// <param name="sourceType">Tipo para busca da interface</param>
+        /// <param name="interfaceType">Tipo da interface para busca</param>
+        /// <returns></returns>
         private static bool HasInterface(Type sourceType, Type interfaceType)
         {
             return sourceType.GetInterfaces()
